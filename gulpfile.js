0edae684a5bcab280e192bfcac896f02a513e8c5
MatheenPasha <pashamatheen@hotmail.com>
2015-02-04 17:06:05 +0000

    added gulp and other build scripts

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+/**
+ * Project gulp file
+ * for all the UI task automation
+ */
+
+// include gulp and all the plug-ins
+var gulp = require('gulp');
+
+// server for the to serve the assets and having liverload
+var server = require('gulp-server-livereload');
+
+
+var del = require('del');
+var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var path = require('path');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+
+// register all the tasks
+
+
+// CSS concat, auto-prefix and minify
+gulp.task('less', ['clean'], function (){
+  return gulp.src('./src/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./src/css'))
+    .pipe(concat('mgallery.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/css/'));
+});
+
+// minify new images
+gulp.task('compressImages', ['clean'], function (){
+  var imgSrc = './src/images/**/*',
+    imgDst = './build/images';
+
+  return gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+
+//concat and minify js
+gulp.task('build-scripts', ['clean'], function (){
+  return gulp.src(['./src/js/*.js'])
+    .pipe(concat('mgallery-min.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js/'));
+});
+
+
+// jslint task to checck the JS
+gulp.task('jshint', function (){
+  return gulp.src('./src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+
+//actual server for the code
+gulp.task('serve', ['build'], function (){
+  gulp.src('./')
+    .pipe(server({
+      port: 5001,
+      directoryListing: false,
+      open: true,
+      log: 'debug',
+      defaultFile: './index.html'
+    }));
+});
+
+gulp.task('clean', function (cb){
+  del([
+    'build/*/**'
+  ], cb);
+});
+
+gulp.task('build', ['jshint', 'build-scripts', 'compressImages', 'less']);
+
+
+gulp.task('watch', function (){
+  gulp.watch('./src/scripts/*.js', ['jshint', 'build-scripts']);
+  gulp.watch('./src/images/**/*', ['compressImages']);
+  gulp.watch('./src/less/*.less', ['less']);
+});
+
+
+//make all task default
+gulp.task('default', ['watch']);
---
+++ src/js/MGallery.js
@@ -0,0 +1,287 @@
+
+  // Actual mGallery source
+
+  (function (window, document, Math){
+
+  var utils = window.IScroll.utils;
+
+  function MGallery(/* element */wrapper, /*object*/ options){
+
+    var mGallery = this;
+    mGallery.isGalleryReady = false;
+
+
+    mGallery.scroller = null;
+    mGallery._events = {};
+    mGallery.currentPageNo = 1;
+    mGallery._galleryDom = wrapper;
+    mGallery.options = {
+      // default carousel settings for best/optimal performance
+      carouselConfig: {
+        'scrollX': true,
+        'scrollY': false,
+        'keyBindings': true,
+        'snap': 'li',
+        'snapSpeed': 500,
+        'snapThreshold': 0.15,
+        'momentum': false,
+        'tap': 'scrollerTap',
+        'zoom': true
+      },
+
+      autoplaygap: 5000,
+
+      controlsGap: 3000,
+
+      autoplaytimer: null,
+
+      autoPlay: false,
+
+      controlsTimer: null,
+
+      noOfImages: null,
+
+      isOpen: false,
+
+      isCLosed: true
+
+    };
+
+    // This list holds all the images.
+    mGallery.viewportList = mGallery._galleryDom.querySelectorAll('ul')[0];
+
+    //list Items
+    mGallery.listItems = mGallery.viewportList.querySelectorAll('li');
+
+    // The close Button 'x' mark
+    mGallery.closeButton = mGallery._galleryDom.querySelectorAll('.close')[0];
+
+    //number of images based on number of list items
+    mGallery.noOfImages = mGallery.listItems.length;
+
+    //all the image tags
+    mGallery.images = mGallery.viewportList.querySelectorAll('img');
+
+    // get all the data
+    mGallery.galleryData = [];
+
+    utils.each(mGallery.images, function (img){
+      mGallery.galleryData.push({
+        'image': img,
+        'mainSrc': utils.getAttr(img, 'mainSrc')
+      });
+    });
+
+
+  }
+
+  MGallery.prototype = {
+
+
+    determineViewPortWidth: function (){
+      return document.documentElement.clientWidth;
+    },
+
+    on: function (type, fn){
+      if (!this._events[type]){
+        this._events[type] = [];
+      }
+
+      this._events[type].push(fn);
+    },
+
+    off: function (type, fn){
+      if (!this._events[type]){
+        return;
+      }
+
+      var index = this._events[type].indexOf(fn);
+
+      if (index > -1){
+        this._events[type].splice(index, 1);
+      }
+    },
+
+    _execEvent: function (type){
+      if (!this._events[type]){
+        return;
+      }
+
+      var i = 0,
+        l = this._events[type].length;
+
+      if (!l){
+        return;
+      }
+
+      for (; i < l; i++){
+        this._events[type][i].apply(this, [].slice.call(arguments, 1));
+      }
+    },
+
+    showControls: function (){
+      var mGallery = this;
+      utils.removeClass(mGallery._galleryDom, 'hide-controls');
+      if (mGallery.controlsTimer !== null){
+        clearTimeout(mGallery.controlsTimer);
+      }
+      mGallery.controlsTimer = setTimeout(function (){
+        mGallery.hideControls();
+      }, mGallery.options.controlsGap);
+    },
+
+    isControlsShowing: function (){
+      var mGallery = this;
+      return !utils.hasClass(mGallery._galleryDom, 'hide-controls');
+    },
+
+    hideControls: function (){
+      var mGallery = this;
+      utils.addClass(mGallery._galleryDom, 'hide-controls');
+      clearTimeout(mGallery.controlsTimer);
+    },
+
+    open: function (){
+      var mGallery = this;
+      utils.removeClass(mGallery._galleryDom, 'hide');
+      utils.addClass(mGallery._galleryDom, 'show');
+      mGallery.onOpen();
+    },
+
+    hide: function (){
+      var mGallery = this;
+      utils.addClass(mGallery._galleryDom, 'hide');
+      utils.removeClass(mGallery._galleryDom, 'show');
+      mGallery.onHide();
+    },
+
+    onHide: function (callback){
+      this.isCLosed = true;
+      this._execEvent('onHide');
+    },
+
+    onOpen: function (){
+      this.isOpen = true;
+
+      //Build the Gallery
+      if (this.isGalleryReady === false){
+        this.buildGallery();
+      }
+      this._execEvent('onOpen');
+    },
+
+    _attachEvents: function (){
+      var mGallery = this;
+      utils.addEvent(mGallery.closeButton, 'click', function (){
+        if (mGallery.isControlsShowing === true){
+          mGallery.hideControls();
+        }
+        mGallery.hide();
+      }, false);
+
+      utils.addEvent(mGallery._galleryDom, 'scrollerTap', function (){
+        if (mGallery.isControlsShowing()){
+          mGallery.hideControls();
+        } else{
+          mGallery.showControls();
+        }
+      }, false);
+
+      mGallery.scroller.on('scrollEnd', function (){
+        mGallery.currentPageNo = mGallery.scroller.currentPage.pageX + 1;
+        mGallery.loadImage(mGallery.currentPageNo);
+      });
+
+      utils.onWindowResize(function() {
+        if(mGallery.isOpen) {
+          mGallery._resize();
+        }
+      });
+    },
+
+
+    positionList: function (){
+      var mGallery = this;
+      // position the the image list according to the screen size
+      mGallery.viewportList.style.padding = '';
+      var padding = Math.ceil(utils.position(mGallery._galleryDom).h / 2) - Math.ceil(utils.position(mGallery.viewportList).h / 2);
+      if (padding < 0){
+        padding = Math.ceil((utils.position(mGallery._galleryDom).h - utils.position(mGallery.viewportList).h) / 2);
+      }
+      mGallery.viewportList.style.padding = padding + 'px 0';
+    },
+
+    refreshList: function (){
+      var mGallery = this;
+      var viewPortWidth = mGallery.determineViewPortWidth();
+      utils.each(mGallery.listItems, function (el){
+        el.style.width = viewPortWidth + 'px';
+      });
+      var listWidth = utils.calculateCombinedWidth(mGallery.listItems);
+      var numberOfWindows = Math.ceil(listWidth / viewPortWidth);
+      mGallery.viewportList.style.width = listWidth + 'px';
+      mGallery.noOfImages = numberOfWindows;
+    },
+
+
+    loadImage: function (n){
+      var mGallery = this;
+      if(mGallery.images[n]) {
+        var handler = function (){
+          utils.removeClass(img, 'loading');
+          utils.addClass(img, 'fade-in');
+        };
+        var img = mGallery.images[n];
+        utils.addClass(img, 'loading');
+        if (img instanceof HTMLImageElement && mGallery.images[n] && utils.getAttr(img, 'src') !== mGallery.galleryData[n].mainSrc){
+          utils.setAttr(img, 'src', mGallery.galleryData[n].mainSrc);
+          utils.addEvent(img, 'load', handler, false);
+        }
+      }
+    },
+
+    getViewPort: function (){
+      return this._galleryDom.querySelectorAll('.viewport')[0];
+    },
+
+    _resize: function (){
+      var mGallery = this;
+      mGallery.refreshList();
+      mGallery.positionList();
+      if (mGallery.scroller === null){
+        mGallery.scroller = new IScroll(mGallery.getViewPort(), mGallery.options.carouselConfig);
+      } else{
+        mGallery.scroller.refresh();
+      }
+      //load the second image.
+      mGallery.loadImage(1);
+      //mGallery.noOfImages > 1 ? mGallery.showButton(gallery.nextButton()) : mGallery.hideButton(gallery.nextButton());
+    },
+
+    buildGallery: function (){
+      var mGallery = this;
+
+      var img = mGallery.images[0];
+      var handler = function (){
+        utils.removeEvent(img, 'load', this);
+        setTimeout(function (){
+          mGallery._resize();
+          //mGallery.windowSlidedListener();
+          utils.addClass(mGallery.getViewPort(), 'fade-in');
+
+          mGallery._attachEvents();
+        }, 1000);
+      };
+      utils.addEvent(img, 'load', handler, false);
+
+      mGallery.loadImage(0);
+      mGallery.isGalleryReady = true;
+    }
+
+
+  };
+
+
+  window.MGallery = MGallery;
+
+})(window, document, Math);
