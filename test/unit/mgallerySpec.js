3c270e5b110fec91616e1feb00d0fff481e5352c
matheenpasha <Mat@Matheens-MacBook-Air.local>
2015-03-01 18:09:04 +0000

    Trying the test suite

--- test/unit/mgallerySpec.js
+++ test/unit/mgallerySpec.js
@@ -1,14 +1,20 @@
 'use strict';
 
+
+
 define(['IScroll', 'MGallery'], function(IScroll, mgallery){
-  describe('Gallery', function () {
 
-    // load the controller's module
-    //beforeEach(angular.mock.module('tui'));
+  describe("MGallery", function() {
 
-    it('should have a HomeCtrl controller', inject(function($controller) {
-      should.exist($controller('HomeCtrl', {$scope: {}}));
-    }));
+    describe("constructor", function() {
+      it("should have a default name", function() {
+        //var gallery = new mgallery();
+        //expect(gallery.scroller).to.equal(null);
+        expect(3).to.equal(3);
+      });
+
+    });
 
   });
+
 });
